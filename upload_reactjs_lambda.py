c6300c96843ad50fdd15ec9a4ba492e06c36ac96
Vasily Pantyukhin <vpantyukhin@gmail.com>
2018-04-07 11:40:19 +0200

    Add exceptions and SNS notifications to the Lambda script

--- upload_reactjs_lambda.py
+++ upload_reactjs_lambda.py
@@ -5,18 +5,29 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
-    targetbucket = s3.Bucket('reactjs.awspyc.com')
     
-    buildbucket = s3.Bucket('reactjsbuild.awspyc.com')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:527104445792:DeployReactJStopic')
     
-    reactjs_zip = StringIO.StringIO()
-    buildbucket.download_fileobj('reactjs.zip', reactjs_zip)
-    
-    with zipfile.ZipFile(reactjs_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            targetbucket.upload_fileobj(obj, nm,
-                ExtraArgs = {'ContentType': mimetypes.guess_type(nm)[0]})
-            targetbucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        targetbucket = s3.Bucket('reactjs.awspyc.com')
+        
+        buildbucket = s3.Bucket('reactjsbuild.awspyc.com')
+        
+        reactjs_zip = StringIO.StringIO()
+        buildbucket.download_fileobj('reactjs.zip', reactjs_zip)
+        
+        with zipfile.ZipFile(reactjs_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                targetbucket.upload_fileobj(obj, nm,
+                    ExtraArgs = {'ContentType': mimetypes.guess_type(nm)[0]})
+                targetbucket.Object(nm).Acl().put(ACL='public-read')
+        
+        print "Job done"
+        topic.publish(Subject='ReactJS', Message='NodeJS deployed Sucessfully')
+    except:
+        topic.publish(Subject='ReactJS', Message='NodeJS deployment Failed')
+        raise
             
     return 'Hello from Lambda'
